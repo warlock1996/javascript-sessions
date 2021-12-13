7f38bf279c8313bbd52b8a1411953b1b4a0e4dc7
Arslan Ali <arslanali.921996@gmail.com>
2021-12-13 09:12:47 +0500

    update: closure docs

--- closures.js
+++ closures.js
@@ -1,7 +1,7 @@
 /*
   1 --> closures are simply some backpack memory attached to function defined inside another function
   2 --> closures give persistent state across multiple function invocations
-  3 --> practical applications can be expensive lookup operations (memoizations) similar to react or v-once in vuejs
+  3 --> practical applications include expensive lookup operations (memoizations) similar to react or v-once in vuejs
 */
 
 function outer() {
