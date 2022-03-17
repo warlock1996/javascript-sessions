e0eaa2bb36d93dd31004dbfcfb11fd83ecec6e51
Arslan Ali <arslanali.921996@gmail.com>
2022-03-17 17:35:19 +0500

    understanding closures again

--- closures.js
+++ closures.js
@@ -4,16 +4,33 @@
   3 --> practical applications include expensive lookup operations (memoizations) similar to react or v-once in vuejs
 */
 
-function outer() {
-  let count = 0;
-  function inner() {
-    count++;
-    console.log('[inner]', count)
-  }
-  function inner2() {
-    count++;
-    console.log('[inner2]', count)
-  }
-  return [inner, inner2];
-}
+// const outer = () => {
+// 	let count = 0
+// 	const inner = () => {
+// 		if (count > 0) {
+// 			console.warn('the component is already rendered !')
+// 		} else {
+// 			console.log(count++)
+// 		}
+// 	}
+// 	return inner
+// }
 
+// const res = outer()
+
+// res()
+// res()
+// res()
+
+// generators example
+// function* generator() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// const gen = generator(); // "Generator { }"
+
+// console.log(gen.next().value); // 1
+// console.log(gen.next().value); // 2
+// console.log(gen.next().value); // 3
